a06ffa3fe8607aed0575675dd22c7bec5793ff68
Tien Phan <tienphan1989@gmail.com>
2020-07-11 01:17:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    let arr = [];
+    for (var i = 0; i<array.length;i++){
+         arr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return arr;
+}
+
+function countDown(n){
+    //let x = 0;
+while (n >= 0) {
+  console.log(n--);
+}
+}
