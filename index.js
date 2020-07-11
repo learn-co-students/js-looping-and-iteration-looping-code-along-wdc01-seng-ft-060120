bd5f502061924f1d4a8b4c66c9c04962d262f103
ianmcknnn <ianmcnnn@gmail.com>
2020-07-11 01:56:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(namesArray){
+	let messages = [];
+	for(name of namesArray){
+		messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+	}
+	return messages;
+}
+
+function countDown(n){
+	for(let i = 0; i <= n; i++){
+		console.log(n - i);
+	}
+}
