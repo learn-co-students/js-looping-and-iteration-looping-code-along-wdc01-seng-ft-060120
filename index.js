0d801e41a87331a06404a38fbf9dbd97e0b71bc0
Diwo777 <diego.zegarra79@icloud.com>
2020-07-11 22:47:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan']
+function writeCards(names,birthday) {
+    let arrOfMessages = []; 
+    for(let i=0; i<names.length;i++){ 
+    arrOfMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`) }
+    return arrOfMessages
+} 
+
+
+function countDown(n) {
+    
+    while(n>=0){console.log(n--)}
+}
